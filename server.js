0c139f7de7c5affa5721373a472160fa0491f6ab
Tanmya Vishvakarma <tanmya2000@gmail.com>
2020-12-31 17:53:27 +0530

    env connect

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
 const express =require('express')
 const app=express()
+const mongoose=require('mongoose')
+const dotenv=require('dotenv')
+
+
+dotenv.config();
+mongoose.connect(process.env.DB_CONNECT,{useNewUrlParser:true},()=>
+console.log("database connected")
+);
+
 
 app.listen(4000,()=>console.log("server running on port 4000"))
