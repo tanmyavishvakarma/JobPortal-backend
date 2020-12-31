1cf2eddae10694974cb0be9701f72af90c49d93f
Tanmya Vishvakarma <tanmya2000@gmail.com>
2020-12-31 18:54:43 +0530

    encypted password

--- routes/routes.js
+++ routes/routes.js
@@ -2,12 +2,16 @@ const { request, response } = require('express')
 const express=require('express')
 const router = express.Router()
 const registertemplatecopy=require('../models/registermodels')
+const bcryt=require('bcrypt')
 
 router.post('/register',async (request,response)=>{
+
+    const saltPassword=await  bcryt.genSalt(10)
+    const securePassword=await bcryt.hash(request.body.password,saltPassword)
     const registeruser=new registertemplatecopy({
         name:request.body.name,
         email:request.body.email,
-        password:request.body.password
+        password:securePassword
 
     })
     registeruser.save()
